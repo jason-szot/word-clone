6860f537f9d9942279f19aa8f9ed409c633c5ea1
coopereric <coopereric70@gmail.com>
2015-12-01 15:07:19 -0600

    Update macro.asm

--- source/macro.asm
+++ source/macro.asm
@@ -7,5 +7,5 @@
 .macro WordArray (%x, %y, %z)
 sll $t8, %z, 2		# num = num / 4
 add $t8, %y, $t8	# %t8 = array[num] (syntax error here. Eric C. fixed.)
-lw %x, ($t7)		# pointer = array[num]
+lw %x, ($t8)		# pointer = array[num] (Updated by Eric C. this should be register $t8)
 .end_macro
