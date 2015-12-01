f51db38e5d0b3a0bb261ba332c3f98c119c2a2ab
coopereric <coopereric70@gmail.com>
2015-12-01 14:54:50 -0600

    Update macro.asm
    line 9 changed %t9 to $t9.

--- source/macro.asm
+++ source/macro.asm
@@ -6,6 +6,6 @@
 # %z is a number to get an entry ( array[z] )
 .macro WordArray (%x, %y, %z)
 sll $t8, %z, 2		# num = num / 4
-add $t8, %y, %t8	# %t8 = array[num]
+add $t8, %y, $t8	# %t8 = array[num] (syntax error here. Eric C. fixed.)
 lw %x, ($t7)		# pointer = array[num]
 .end_macro
